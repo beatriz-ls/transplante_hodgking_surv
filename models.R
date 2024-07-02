d6ca92b307dac60e0d9d803efaaa09528e526b12
Beatriz Lima <82064428+beatriz-ls@users.noreply.github.com>
2024-07-02 17:00:18 -0300

    Add files via upload

--- models.R
+++ models.R
@@ -4,6 +4,7 @@ library(survival)
 library(readr)
 library(survminer)
 library(data.table)
+library(lmtest)
 
 # Data
 
@@ -11,20 +12,39 @@ dt <- read_delim("HOD_NHL.csv", delim = ";",
                  escape_double = FALSE, trim_ws = TRUE) |> as.data.table()
 
 
-dt[, Karnofsky_cat := ifelse(Karnofsky >= 80, ">= 80", "< 80")]
+dt[, Karnofsky_cat := ifelse(Karnofsky < 80,"< 80",">= 80")]
 dt[, Karnofsky := NULL]
-dt[, Graft := NULL]
 
 
 # Weibull
 
-model_weibull <- survreg(Surv(Time, D_R) ~ ., data = dt[, Karnofsky := NULL], dist = "weibull")
-model_weibull1 <- survreg(Surv(Time, D_R) ~ ., data = dt, dist = "weibull")
+model_weibull <- survreg(Surv(Time, D_R) ~ ., data = dt,
+                         dist = "weibull")
 # Exponencial
 
 model_exp <- survreg(Surv(Time, D_R) ~ ., data = dt, dist = "exponential")
 
-# Taxa de falha acumulada
+# Riscos proporcionais no modelo weibull
 
-taxa_weibull <- basehaz(model_weibull)
+# Risco de transplante alogenico sobre transplante autologo
+risco_graft <- exp(-model_weibull[["coefficients"]][["Graft"]])^(1/model_weibull[["scale"]])
+
+# Risco de escore de Karnofsky bom sobre escore ruim
+risco_escore <- exp(-model_weibull[["coefficients"]][["Karnofsky_cat>= 80"]])^(1/model_weibull[["scale"]])
+
+# Modelo Log Logistica
+
+model_log <-survreg(Surv(Time,D_R) ~., data = dt,
+                    dist = "loglogistic")
+
+# Razão de chances no modelo log logistico
+
+# Razão de chance de transplante aloginico sobre transplante autólogo
+odds_graft <- exp(-model_log[["coefficients"]][["Graft"]]/model_log[["scale"]])
+
+odds_escore <- exp(-model_log[["coefficients"]][["Karnofsky_cat>= 80"]]/model_log[["scale"]])
+
+
+
+
 
