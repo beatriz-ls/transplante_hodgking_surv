59a14d54b473d34400fd17e155f87b99cdf881e4
Beatriz Lima <82064428+beatriz-ls@users.noreply.github.com>
2024-07-03 15:55:48 -0300

    Add files via upload

--- models.R
+++ models.R
@@ -11,10 +11,8 @@ library(lmtest)
 dt <- read_delim("HOD_NHL.csv", delim = ";",
                  escape_double = FALSE, trim_ws = TRUE) |> as.data.table()
 
-
 dt[, Karnofsky_cat := ifelse(Karnofsky < 80,"< 80",">= 80")]
 dt[, Karnofsky := NULL]
-
 
 # Weibull
 
@@ -45,6 +43,19 @@ odds_graft <- exp(-model_log[["coefficients"]][["Graft"]]/model_log[["scale"]])
 odds_escore <- exp(-model_log[["coefficients"]][["Karnofsky_cat>= 80"]]/model_log[["scale"]])
 
 
+# Teste de wald para ver se beta é diferente de 0 para weibull
 
+# Graft
+(model_weibull[["coefficients"]][["Graft"]]^2)/(0.525)^2
 
+# Escore
+(model_weibull[["coefficients"]][["Karnofsky_cat>= 80"]]^2)/(0.563)^2
+
+# Teste de wald para ver se beta é diferente de 0 para log logistica
+
+# Graft
+(model_log[["coefficients"]][["Graft"]]^2)/(0.6519)^2
+
+# Escore
+(model_log[["coefficients"]][["Karnofsky_cat>= 80"]]^2)/(0.6480)^2
 
